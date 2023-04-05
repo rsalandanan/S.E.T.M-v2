25812815ddb01cb8a61c48408dc7137f19335061
duwpo <raphaelyapsalandanan@gmail.com>
2023-04-05 13:27:32 +0800

    jump

--- S.E.T.M v2/Assets/Scripts/PlayerController.cs
+++ S.E.T.M v2/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,10 +6,15 @@ public class PlayerController : MonoBehaviour
   [SerializeField] private float mouseSens;
   [SerializeField] [Range(0.0f, 0.05f)] private float cameraSmooth = 0.3f;
   [SerializeField] private bool lockCursor = true;
+  [SerializeField] private float walkSpeed;
   Vector2 _currentMouseDelta = Vector2.zero;
   private Vector2 _currentMouseDeltaVelocity = Vector2.zero;
   private CharacterController _controller;
   private float _cameraPitch;
+
+  private float _gravity = -9.81f;
+  private float _velocityY;
+  private float _jumpForce = 10f;
 
   private void Start()
   {
@@ -26,6 +30,7 @@ public class PlayerController : MonoBehaviour
   private void Update()
   {
     MouseLook();
+    Movement();
   }
 
   void MouseLook()
@@ -36,5 +41,29 @@ public class PlayerController : MonoBehaviour
     _cameraPitch = Mathf.Clamp(_cameraPitch, -75.0f, 75.0f);
     playerCamera.localEulerAngles = Vector3.right * _cameraPitch;
     transform.Rotate(Vector3.up *(_currentMouseDelta.x * mouseSens));
+  }
+
+  void Movement()
+  {
+    Vector2 targetDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    targetDirection.Normalize();
+    if (_controller.isGrounded)
+    {
+      _velocityY = 0.0f;
+    }
+    else
+    {
+      _velocityY += _gravity * Time.deltaTime;
+    }
+
+    if (_controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+    {
+      _velocityY += _jumpForce;
+    }
+
+    var transform1 = transform;
+    Vector3 velocity = (transform1.forward * targetDirection.y + transform1.right * targetDirection.x) * walkSpeed +
+                       Vector3.up * _velocityY;
+    _controller.Move(velocity * Time.deltaTime);
   }
 }
